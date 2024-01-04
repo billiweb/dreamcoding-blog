878369dbd5fa2f5fe98f6bce4c89de8a2c33869d
heejin_song <song_heejin@naver.com>
2024-01-04 22:10:40 +0900

    markdown 적용

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,7 @@
+import MarkdownViewer from "@/components/MarkdownViewer";
 import { getPostData } from "@/service/posts";
+import Image from "next/image";
+import { AiTwotoneCalendar } from "react-icons/ai";
 
 type Props = {
   params: {
@@ -7,12 +10,26 @@ type Props = {
 };
 
 export default async function Postpage({ params: { slug } }: Props) {
-  const post = await getPostData(slug);
+  const { title, description, date, path, content } = await getPostData(slug);
 
   return (
-    <>
-      <h1>{post.title}</h1>
-      <pre>{post.content}</pre>
-    </>
+    <article className="rounded-2xl overflow-hiddenm-4 p-4">
+      <Image
+        className="w-full max-h-[500px]"
+        src={`/images/posts/${path}.png`}
+        alt={title}
+        width={760}
+        height={420}
+      />
+      <section className="flex flex-col p-4">
+        <div className="flex items-center self-end">
+          <AiTwotoneCalendar />
+          <p className="font-semibold ml-2">{date.toString()}</p>
+        </div>
+        <h1 className="text-4xl font-bold">{title}</h1>
+        <p className="text-xl font-bold">{description}</p>
+        <MarkdownViewer content={content} />
+      </section>
+    </article>
   );
 }
